f3a10732fada5ae8d4e2e2c197ee1df14da18f81
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-10-16 19:34:44 +0200

    Section + font

--- Web/src/screens/concerts/ConcertsScreen.jsx
+++ Web/src/screens/concerts/ConcertsScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { PastConcertsList, NextConcertList, Header } from "../../components";
+import Section from "../../components/section/Section";
 
 function ConcertsScreen() {
   const [status, setStatus] = useState(true);
@@ -16,12 +17,11 @@ function ConcertsScreen() {
   return (
     <>
       <Header />
-
+      <Section title="Concerts" icon="edit">
       <button onClick={handleNextConcerts}>Next Concerts</button>
-
       <button onClick={handlePastConcerts}>Past Concerts</button>
-
       <div>{status ? <NextConcertList /> : <PastConcertsList />}</div>
+      </Section>
     </>
   );
 }
