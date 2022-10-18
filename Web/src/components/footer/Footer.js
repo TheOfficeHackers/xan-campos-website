46e952da3451b3ccb1a023093ef56e536d0da760
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-10-18 22:15:51 +0200

    Contact form CSS & work item minors

--- Web/src/components/footer/Footer.js
+++ Web/src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import footerpic from "../../images/footer.png";
 function Footer() {
   return (
     <div className="position-relative">
-      <img src={footerpic} alt="footer " />
+      <img src={footerpic} alt="footer"  style={{width: "100%" }}/>
       <div className="text-center text-light fs-2 position-absolute top-50 start-50 translate-middle">
         <div className="mb-3">
           <span> Join the Newsletter </span>
--- Web/src/screens/contact/ContactScreen.jsx
+++ Web/src/screens/contact/ContactScreen.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { Header } from '../../components'
 import ContactForm from '../../components/contact-form/ContactForm'
 import contactpic from "../../images/contact-pic.png";
+import Footer from "../../components/footer/Footer";
 
 
 function ContactScreen() {
   return (
     <>
       <Header />
-      <div><img src={contactpic} alt="xan-campos" className="position-relative" />
+      <div><img src={contactpic} alt="xan-campos" className="position-relative" style={{width: "100%" }}/>
       <div class="position-absolute top-50 start-50 translate-middle"><ContactForm /></div>
       
       </div>
-      
+      <Footer />
     </>
   )
 }
