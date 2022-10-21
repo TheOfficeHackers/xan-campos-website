6a08ab1043d467fecc5fbf7afb7adcdb90289d6b
marialalin <mariavencesantorum@gmail.com>
2022-10-21 19:19:08 +0200

    footer & contact pics

--- Web/src/screens/contact/ContactScreen.jsx
+++ Web/src/screens/contact/ContactScreen.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Header } from '../../components'
+import { CoverPicture, Header } from '../../components'
 import ContactForm from '../../components/contact-form/ContactForm'
-import contactpic from "../../images/contact-pic.png";
+
 
 
 
@@ -9,9 +9,8 @@ function ContactScreen() {
   return (
     <>
       <Header />
-      <div><img src={contactpic} alt="xan-campos" className="position-relative" style={{width: "100%" }}/>
+      <CoverPicture src="https://res.cloudinary.com/dyl3cklgp/image/upload/v1666372245/xan-campos-web/contact-pic-def_pb1xvn.png"/>
       <div class="position-absolute top-50 start-50 translate-middle"><ContactForm /></div>
-      </div>
     </>
   )
 }
