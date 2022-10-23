09ce0cb037a82cf232e89a0c51691f9950bb03c1
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-10-23 18:34:04 +0200

    Errors screen

--- Web/src/screens/errors/ErrorsScreen.jsx
+++ Web/src/screens/errors/ErrorsScreen.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { CoverPicture } from "../../components";
 
 function ErrorsScreen() {
   return (
-    <div>ErrorsScreen</div>
-  )
+    <>
+      <Link to="" className="text-decoration-none">
+      <CoverPicture src="https://res.cloudinary.com/dyl3cklgp/image/upload/v1666541339/xan-campos-web/10-error-404-page-examples-for-UX-design_dyvfsx.png" />
+      </Link>
+    </>
+  );
 }
 
-export default ErrorsScreen
+export default ErrorsScreen;
