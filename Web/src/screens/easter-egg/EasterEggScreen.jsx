295bf508a927fed7241a95ed9b0d716b14d6eafa
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-10-25 01:19:57 +0200

    minor

--- Web/src/screens/easter-egg/EasterEggScreen.jsx
+++ Web/src/screens/easter-egg/EasterEggScreen.jsx
@@ -10,19 +10,19 @@ function EasterEggScreen() {
         <h1>Un viaje fantástico</h1>
         <br></br>
         <h4>
-          En abril de 2022, un grupo de intrépidos fans de Agoney decide
+          En abril de 2022, un grupo de intrépid@s fans de Agoney decide
           contratar a los formadores más estrictos de la deep web para aprender
           a mover ranas de sitio y otras proezas que no hace ni Harry.</h4>
           <div className="my-4">
           <img style={{maxHeight:"500px"}}src="https://res.cloudinary.com/dyl3cklgp/image/upload/v1666652648/xan-campos-web/Easter%20Egg/nasiospamatar_ak7hgm.png"></img>
           </div>
          <h4>Tras seis árduos meses de rellenar encuestas hasta el vómito y algún
-          que otro lab empezado, estos talentosos hackers se disponen a
+          que otro lab empezado, est@s talentos@s hackers se disponen a
           aterrizar en el sector para parasitar durante años a costa de alguna
           empresa incauta (telefónica, quizás?).<br></br>
           <br></br>
           Si tiene usted algún problema informático o psicosesuarl y
-          se los encuentra, quizá pueda contratarlos... Ell@s son los...
+          se l@s encuentra, quizá pueda contratarl@s... Ell@s son los...
         </h4>
         <div style={{height:"50px"}}></div>
         <h3>WDPT APR'22 IRONMASTERS</h3>
