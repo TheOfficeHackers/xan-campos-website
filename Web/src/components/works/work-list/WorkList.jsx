cfcbbfc23a04131cbe3a99e6d6244e0441ef10fe
marialalin <mariavencesantorum@gmail.com>
2022-10-24 21:33:27 +0200

    loading

--- Web/src/components/works/work-list/WorkList.jsx
+++ Web/src/components/works/work-list/WorkList.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { getWorksXanTrio, getWorksXan } from "../../../services/website-service";
+import {
+  getWorksXanTrio,
+  getWorksXan,
+} from "../../../services/website-service";
 import WorkItem from "../work-item/WorkItem";
-
 
 function WorkList({ className }) {
   const [worksXanTrio, setWorksXanTrio] = useState(null);
@@ -24,23 +26,37 @@ function WorkList({ className }) {
   return (
     <div>
       <div className={className}>
-        {worksXanTrio
-          ? worksXanTrio.map((work) => (
-              <div className="" key={work.id}>
-                <WorkItem {...work} />
-              </div>
-            ))
-          : "Loading..."}
+        {worksXanTrio ? (
+          worksXanTrio.map((work) => (
+            <div className="" key={work.id}>
+              <WorkItem {...work} />
+            </div>
+          ))
+        ) : (
+          <div className="m-0 vh-100 row justify-content-center">
+            <div className="col-auto">
+              <img
+                className=" justify-content-center"
+                src="https://media.giphy.com/media/FMW20upY7JfTRfhVq7/giphy.gif" style={{width:"40rem"}}
+                alt="loading"
+              />
+            </div>
+          </div>
+        )}
       </div>
 
       <div className={className}>
-        {worksXan
-          ? worksXan.filter((work) => work.title === "ESD").map((work) => (
+        {worksXan ? (
+          worksXan
+            .filter((work) => work.title === "ESD")
+            .map((work) => (
               <div className="" key={work.id}>
                 <WorkItem {...work} />
               </div>
             ))
-          : <></>}
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
